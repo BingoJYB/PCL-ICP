0010c7dc12432293c4b0c16e677ec29c54e03bb6
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-11-09 01:45:29 +0100

    add noise

--- example.py
+++ example.py
@@ -43,49 +43,78 @@ def get_center(rectangle):
     return center
 
 
-def rotate(rectangle, angle, pivot=(0, 0)):
+# def rotate(rectangle, angle, pivot=(0, 0)):
+#     theta = (np.pi / 180.0) * angle
+#     a0, b0 = np.cos(theta), np.sin(theta)
+#     a1, b1 = pivot[0] - pivot[0] * a0 + pivot[1] * b0,\
+#         pivot[1] - pivot[0] * b0 - pivot[1] * a0
+
+#     R = np.array([[a0, -b0, a1],
+#                   [b0, a0, b1],
+#                   [0, 0, 1]])
+
+#     return np.dot(rectangle, R.T)
+
+
+# def translate(rectangle, delta_x=0, delta_y=0):
+#     T = np.array([[1, 0, delta_x],
+#                   [0, 1, delta_y],
+#                   [0, 0, 1]])
+
+#     return np.dot(rectangle, T.T)
+
+
+def rotate(rectangle, angle):
     theta = (np.pi / 180.0) * angle
-    a0, b0 = np.cos(theta), np.sin(theta)
-    a1, b1 = pivot[0] - pivot[0] * a0 + pivot[1] * b0,\
-        pivot[1] - pivot[0] * b0 - pivot[1] * a0
+    a0, b0 = np.math.cos(theta), np.math.sin(theta)
+    rot = np.array([[a0, -b0],
+                    [b0, a0]])
 
-    R = np.array([[a0, -b0, a1],
-                  [b0, a0, b1],
-                  [0, 0, 1]])
-
-    return np.dot(rectangle, R.T)
+    return np.dot(rectangle, rot)
 
 
 def translate(rectangle, delta_x=0, delta_y=0):
-    T = np.array([[1, 0, delta_x],
-                  [0, 1, delta_y],
-                  [0, 0, 1]])
+    translated_rect = rectangle.copy()
+    translated_rect += (delta_x, delta_y)
 
-    return np.dot(rectangle, T.T)
+    return translated_rect
+
+
+def add_noise(rectangle, num=0, margin=0):
+    left, right = np.min(rectangle[:, 0]), np.max(rectangle[:, 0])
+    bottom, top = np.min(rectangle[:, 1]), np.max(rectangle[:, 1])
+    factor_x = right - left + 2 * margin
+    factor_y = top - bottom + 2 * margin
+
+    noise = np.random.rand(num, 2)
+    noise = noise * (factor_x, factor_y) + (left, bottom)
+
+    return np.concatenate((rectangle, noise), axis=0)
 
 
 def main():
     left_top_vertex = (1, 21)
     right_bottom_vertex = (21, 11)
-    rotation_angle = 45
-    delta_x, delta_y = 30, 15
+    rotation_angle = 0
+    delta_x, delta_y = 1, 1
 
     rectangle = get_rectangle(left_top_vertex, right_bottom_vertex)
-    homo_rect = get_homo_coord(rectangle)
-    center = get_center(rectangle)
-    transformed_homo_rect = homo_rect.copy()
-    transformed_homo_rect = rotate(
-        transformed_homo_rect, rotation_angle, center)
-    transformed_homo_rect = translate(transformed_homo_rect, delta_x, delta_y)
-    transformed_rect = dehomo(transformed_homo_rect)
+    # homo_rect = get_homo_coord(rectangle)
+    # center = get_center(rectangle)
+    # transformed_homo_rect = homo_rect.copy()
+    transformed_rect = rectangle.copy()
+    transformed_rect = rotate(transformed_rect, rotation_angle)
+    transformed_rect = translate(transformed_rect, delta_x, delta_y)
+    # transformed_rect = dehomo(transformed_homo_rect)
+    transformed_rect_with_noise = add_noise(transformed_rect, 15, 2)
 
     transformation_history, aligned_points = icp(
-        rectangle, transformed_rect, verbose=True)
+        transformed_rect_with_noise, rectangle, point_pairs_threshold=1, verbose=True)
 
     x = rectangle[:, 0]
     y = rectangle[:, 1]
-    xx = transformed_rect[:, 0]
-    yy = transformed_rect[:, 1]
+    xx = transformed_rect_with_noise[:, 0]
+    yy = transformed_rect_with_noise[:, 1]
     xxx = aligned_points[:, 0]
     yyy = aligned_points[:, 1]
 
